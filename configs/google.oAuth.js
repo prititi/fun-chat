f6eabeeb1e75c1f821f3eff248348663aa6016e7
prititi <pt446914@gmail.com>
2023-04-03 00:52:38 +0530

    deploy

--- configs/google.oAuth.js
+++ configs/google.oAuth.js
@@ -9,7 +9,7 @@ var GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
 passport.use(new GoogleStrategy({
     clientID:    process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8500/oauth/google/callback",
+    callbackURL: "https://fun-chat-ht6d.onrender.com/oauth/google/callback",
   
   },
  function(request, accessToken, refreshToken, profile,cb) {
